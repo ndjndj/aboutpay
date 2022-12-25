98acfacb52ab51b3eba790eb498374af2d9becf3
ndjndj <teq.ndj@gmail.com>
2022-12-26 00:27:45 +0900

    f

---
+++ aboutpay/lib/constants/route_bottom_nav_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RouteBottomNavBar {
+  const RouteBottomNavBar._();
+
+  static const List<Widget> pages = [];
+  static const List<String> labels = [];
+  static const List<Widget> icons = [];
+}
