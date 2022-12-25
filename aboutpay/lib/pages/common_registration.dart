38b9f59e5041b4c6e9aac66c6da7d368a80b5348
ndjndj <teq.ndj@gmail.com>
2022-12-26 00:39:38 +0900

    f

--- aboutpay/lib/pages/common_registration.dart
+++ aboutpay/lib/pages/common_registration.dart
@@ -7,17 +7,36 @@ class CommonRegistration extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      decoration: BoxDecoration(
+        color: const Color(0xFF333333).withOpacity(0.4),
+        borderRadius: BorderRadius.circular(10.0)
+      ),
       child: Column(
         children: [
-          const Text('Registration'),
           Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
+            child: const Text(
+              'Registration',
+              style: TextStyle(
+                fontSize: 24,
+                color: Color(0xFFFAFAFA)
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: TextField(
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Color(0xFFFAFAFA)
               ),
             ),
-          )
+          ),
+
         ],
       ),
     );
